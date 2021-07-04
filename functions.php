be26d16789bb0c95bd908a7daaeeef6406771efe
IRS-dev <irsyadalyahya5@gmail.com>
2021-07-04 18:08:28 +0700

    regristrasi aman

---
+++ functions.php
@@ -0,0 +1,72 @@
+<?php
+// Koneksi ke database
+$conn = new mysqli("localhost", "root", "", "login-native");
+
+function query($query)
+{
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+
+
+
+
+function registrasi($data)
+{
+
+    global $conn;
+
+    $username = strtolower(stripslashes($data["username"]));
+    $email = ($data["email"]);
+    $password = mysqli_real_escape_string($conn, $data["pass"]);
+    $password2 = mysqli_real_escape_string($conn, $data["pass2"]);
+
+
+    $namescheck = "SELECT username FROM users WHERE username='$username'";
+    //cek username sudah ada atau belum
+    $result = mysqli_query($conn, $namescheck);
+
+    if (mysqli_fetch_assoc($result)) {
+        echo "
+        <script>
+        alert('username is already exsist!')
+        
+        </script>
+        
+        ";
+
+        return false;
+    }
+
+
+
+
+    //cek konfirmasi password
+    if ($password !== $password2) {
+
+        echo "
+        <script>
+        alert('somethings wrong with your password');
+
+        </script>";
+
+
+        return false;
+    }
+    //enkripsi password
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+
+    //tambahkan userbaru ke databases
+    mysqli_query($conn, "INSERT INTO users VALUE(NULL,'$username','$email','$password')");
+
+
+    return mysqli_affected_rows($conn);
+}
---
+++ signIn.php
@@ -0,0 +1,130 @@
+<?php
+require_once 'functions.php';
+
+
+
+if (isset($_POST["register"])) {
+    $data = $_POST;
+
+
+    if (registrasi($_POST) > 0) {
+
+        echo "
+                    <script>
+                    alert('Hi! New Usser!');
+
+                    </script>";
+    } else {
+        echo mysqli_error($conn);
+    }
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Login PHP Native</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <!--===============================================================================================-->
+</head>
+
+<body>
+
+    <div class="limiter">
+        <div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+            <div class="wrap-login100">
+                <form class="login100-form validate-form" action="signIn.php" method="POST">
+                    <span class="login100-form-logo">
+                        <i class="zmdi zmdi-landscape"></i>
+                    </span>
+
+                    <span class="login100-form-title p-b-34 p-t-27">
+                        Sign In
+                    </span>
+
+                    <!-- ussername -->
+                    <div class="wrap-input100 validate-input" data-validate="Enter username">
+                        <input class="input100" type="text" name="username" placeholder="Username" autofocus id="username">
+                        <span class="focus-input100" data-placeholder="&#xf207;" for="username"></span>
+                    </div>
+                    <!-- email -->
+                    <div class="wrap-input100 validate-input" data-validate="Enter username">
+                        <input class="input100" type="email" name="email" placeholder="Email">
+                        <span class="focus-input100" data-placeholder="&#xf207;"></span>
+                    </div>
+                    <!-- password 1 -->
+                    <div class="wrap-input100 validate-input" data-validate="Enter password">
+                        <input class="input100" type="password" name="pass" placeholder="Password">
+                        <span class="focus-input100" data-placeholder="&#xf191;"></span>
+                    </div>
+                    <!-- password 2 -->
+                    <div class="wrap-input100 validate-input" data-validate="Enter password">
+                        <input class="input100" type="password" name="pass2" placeholder="Comfirmation your password">
+                        <span class="focus-input100" data-placeholder="&#xf191;"></span>
+                    </div>
+
+                    <div class="container-login100-form-btn">
+                        <button class="login100-form-btn" type="submit" name="register">
+                            Sign In
+                        </button>
+                    </div>
+
+
+                    <div class="text-center p-t-90">
+                        <a class="txt1" href="login.php">
+                            Already have an account?
+                        </a>
+
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+    <div id="dropDownSelect1"></div>
+
+    <!--===============================================================================================-->
+    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/animsition/js/animsition.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/bootstrap/js/popper.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/daterangepicker/moment.min.js"></script>
+    <script src="vendor/daterangepicker/daterangepicker.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/countdowntime/countdowntime.js"></script>
+    <!--===============================================================================================-->
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
